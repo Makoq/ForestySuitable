3ee114d52a49691def054a899a30ae563b290835
Makoq <913088741@qq.com>
2019-07-24 16:57:07 +0800

    popup display

--- src/page/Map.vue
+++ src/page/Map.vue
@@ -1,9 +1,11 @@
 <template>
-
-   
     <div id="map" ref="mapcontainer"  >
-       <div id="nodelist">info</div>
-       
+       <h1 style="margin-left:5%;position:fixed;z-index:2"><strong>Foresty Suitable Land Web System</strong></h1>
+       <!-- <div id="nodelist"></div> -->
+        <div id="popup" class="ol-popup">
+        <a href="#" id="popup-closer" class="ol-popup-closer"></a>
+        <div id="popup-content"></div>
+        </div>
     </div>
      
   
@@ -17,9 +19,11 @@ import TileLayer from 'ol/layer/Tile';
 // import XYZ from 'ol/source/XYZ';
 import TileWMS from 'ol/source/TileWMS';
 import OSM from 'ol/source/OSM';
+ 
 //ScaleLine
 import {defaults as defaultControls,ScaleLine} from 'ol/control.js';
 import 'ol/ol.css';
+import Overlay from 'ol/Overlay';
 
 export default {
  
@@ -35,7 +39,27 @@ export default {
   },
   
   mounted(){
+      
+      var container = document.getElementById('popup');
+      var content = document.getElementById('popup-content');
+      var closer = document.getElementById('popup-closer');
+
+    var overlay = new Overlay({
+            element: container,
+            autoPan: true,
+            autoPanAnimation: {
+            duration: 250
+            }
+        });
+    closer.onclick = function() {
+            overlay.setPosition(undefined);
+            closer.blur();
+            return false;
+        };
+
+
        this.map=new Map({
+        overlays: [overlay],
         target:'map',
         controls: defaultControls().extend([
                             
@@ -88,11 +112,12 @@ export default {
        //添加地图控件
        this.map.addControl(new ScaleLine());
 
-
         let that=this
         this.map.on('singleclick', function(evt) {
 
-        document.getElementById('nodelist').innerHTML = "Loading... please wait...";
+         var coordinate = evt.coordinate;   
+
+        // document.getElementById('nodelist').innerHTML = "Loading... please wait...";
         
         var view = that.map.getView();
         var viewResolution = view.getResolution();
@@ -103,10 +128,10 @@ export default {
           {'INFO_FORMAT': 'text/html', 'FEATURE_COUNT': 50});
 
         if (url) {
-          document.getElementById('nodelist').innerHTML = '<iframe seamless style="width:800px;border:none;margin-left:50%" src="' + url + '"></iframe>';
+        //   document.getElementById('nodelist').innerHTML = '<iframe seamless id="iframe" style="width:800px;height:200px;border:none;margin-left:10%" src="' + url + '"></iframe>';
         }
-       
-   
+        content.innerHTML= '<div style="height:100% auto;width:100% auto "> <iframe seamless id="iframe" border:none;margin-left:10%" src="' + url + '"></iframe></div>';
+        overlay.setPosition(coordinate);
 
       });
 
@@ -131,12 +156,64 @@ export default {
 #map .ol-overlaycontainer-stopevent {
     display: none;
 }
-#nodelist{
-    z-index: 2;
-    /* height: 200px;
-    width: 800px;  */
+/* #nodelist{
+    z-index: 1;
+    height: 100px;
+    width: 800px; 
+    background: white;
+    opacity: .7;
+    margin-left: 45%;
+    margin-top: 0;
     position: fixed;   
+    -moz-outline-radius: .6;
      
+} */
+#nodelist:hover{
+    opacity: 1;
 }
  
+
+
+ .ol-popup {
+        position: absolute;
+        background-color: white;
+        -webkit-filter: drop-shadow(0 1px 4px rgba(0,0,0,0.2));
+        filter: drop-shadow(0 1px 4px rgba(0,0,0,0.2));
+        padding: 15px;
+        border-radius: 10px;
+        border: 1px solid #cccccc;
+        bottom: 12px;
+        left: -50px;
+        min-width: 280px;
+      }
+      .ol-popup:after, .ol-popup:before {
+        top: 100%;
+        border: solid transparent;
+        content: " ";
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+      }
+      .ol-popup:after {
+        border-top-color: white;
+        border-width: 10px;
+        left: 48px;
+        margin-left: -10px;
+      }
+      .ol-popup:before {
+        border-top-color: #cccccc;
+        border-width: 11px;
+        left: 48px;
+        margin-left: -11px;
+      }
+      .ol-popup-closer {
+        text-decoration: none;
+        position: absolute;
+        top: 2px;
+        right: 8px;
+      }
+      .ol-popup-closer:after {
+        content: "✖";
+      }
 </style>
